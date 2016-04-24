2b23b53df17dc57f77c95bfee93d9f89873d923a
Javier <javicho21@gmail.com>
2016-04-23 23:42:55 -0300

    ajuste TAGs : -> =

--- speed.py
+++ speed.py
@@ -13,7 +13,7 @@ client.connect("localhost", 1883, 60)
 
 time_stamp = int(time.time()*1000)
 
-arq = open('/home/azhang/speedtest/speedtest.txt', 'r')
+arq = open('/home/azhang/jarova/python/speed-service/speedtest.txt', 'r')
 textoPing = arq.readline()
 textoDownload = arq.readline()
 textoUpload = arq.readline()
@@ -28,21 +28,21 @@ dataType = "METRIC"
 
 #msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"Raspberry.id":"%s","rpi.hostname": "%s","rpi.ip": "%s","rpi.datatype": "%s", "sensor.name": "%s","sensor.unit": "%s"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Latency",device,RPIhostname,RPIip,dataType,"Ping",testPingList[2],time_stamp,testPingList[1])
 
-msg ='%s,Raspberry.id:%s,rpi.hostname:%s,rpi.ip:%s,rpi.datatype:%s,sensor.unit:%s,sensor.name:%s value=%s %s' % ("Latency",device,RPIhostname,RPIip,dataType,"ms","Ping",testPingList[1],time_stamp)
+msg ='%s,Raspberry.id=%s,rpi.hostname=%s,rpi.ip=%s,rpi.datatype=%s,sensor.unit=%s,sensor.name=%s value=%s %s' % ("Latency",device,RPIhostname,RPIip,dataType,"ms","Ping",testPingList[1],time_stamp)
 print msg
 client.publish("javier/board1", msg)
 time.sleep(0.1)
 
 #msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"Raspberry.id":"%s","rpi.hostname": "%s","rpi.ip": "%s","rpi.datatype": "%s", "sensor.name": "%s","sensor.unit": "%s"},\nvalues: {"%s":"%s"}\n}]\n}' % ("DownloadSpeed",device,RPIhostname,RPIip,dataType,"DownloadSpeed",testDownloadList[2],time_stamp,testDownloadList[1])
 
-msg ='%s,Raspberry.id:%s,rpi.hostname:%s,rpi.ip:%s,rpi.datatype:%s,sensor.unit:%s,sensor.name:%s value=%s %s' % ("DownloadSpeed",device,RPIhostname,RPIip,dataType,"Mbit/s","DownloadSpeed",testDownloadList[1],time_stamp)
+msg ='%s,Raspberry.id=%s,rpi.hostname=%s,rpi.ip=%s,rpi.datatype=%s,sensor.unit=%s,sensor.name=%s value=%s %s' % ("DownloadSpeed",device,RPIhostname,RPIip,dataType,"Mbit/s","DownloadSpeed",testDownloadList[1],time_stamp)
 print msg
 client.publish("javier/board1", msg)
 time.sleep(0.1)
 
 #msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"Raspberry.id":"%s","rpi.hostname": "%s","rpi.ip": "%s","rpi.datatype": "%s", "sensor.name": "%s","sensor.unit": "%s"},\nvalues: {"%s":"%s"}\n}]\n}' % ("UploadSpeed",device,RPIhostname,RPIip,dataType,"UploadSpeed",testUploadList[2],time_stamp,testUploadList[1])
 
-msg ='%s,Raspberry.id:%s,rpi.hostname:%s,rpi.ip:%s,rpi.datatype:%s,sensor.unit:%s,sensor.name:%s value=%s %s' % ("UploadSpeed",device,RPIhostname,RPIip,dataType,"Mbit/s","UploadSpeed",testUploadList[1],time_stamp)
+msg ='%s,Raspberry.id=%s,rpi.hostname=%s,rpi.ip=%s,rpi.datatype=%s,sensor.unit=%s,sensor.name=%s value=%s %s' % ("UploadSpeed",device,RPIhostname,RPIip,dataType,"Mbit/s","UploadSpeed",testUploadList[1],time_stamp)
 print msg
 client.publish("javier/board1", msg)
 time.sleep(0.1)
